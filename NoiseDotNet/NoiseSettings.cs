0388f67dbc191224f288fcf1a4556e9fea09d052
Miles <krubbles@gmail.com>
2026-02-21 12:02:23 -0800

    New NoiseSettings constructors

--- NoiseDotNet/NoiseSettings.cs
+++ NoiseDotNet/NoiseSettings.cs
@@ -39,6 +39,22 @@ namespace NoiseDotNet
         /// </summary>
         public int Seed;
 
+        public NoiseSettings(float xFrequency, float yFrequency, float zFrequency, float amplitude, float amplitude2, int seed)
+        {
+            XFrequency = xFrequency;
+            YFrequency = yFrequency;
+            ZFrequency = zFrequency;
+            Amplitude = amplitude;
+            Amplitude2 = amplitude2;
+            Seed = seed;
+        }
+
+        public NoiseSettings(float xFrequency, float yFrequency, float amplitude, float amplitude2, int seed) : this(xFrequency, yFrequency, 0f, amplitude, amplitude2, seed) { }
+
+        public NoiseSettings(float xFrequency, float yFrequency, int seed) : this(xFrequency, yFrequency, 0f, 1f, 1f, seed) { }
+
+        public NoiseSettings(float xFrequency, float yFrequency, float zFrequency, int seed) : this(xFrequency, yFrequency, zFrequency, 1f, 1f, seed)  { }
+
         public void Deconstruct(out float xFrequency, out float yFrequency, out float zFrequency, out float amplitude, out float amplitude2, out int seed)
         {
             xFrequency = XFrequency;
