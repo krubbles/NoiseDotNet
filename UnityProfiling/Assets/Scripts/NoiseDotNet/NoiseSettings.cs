3a9cdd39b22bc242afa21233a6976b40d74cf8b4
Miles <krubbles@gmail.com>
2026-02-21 11:42:00 -0800

    Synced unity noisedotnet with parent noisedotnet, moved into scripts folder

---
+++ UnityProfiling/Assets/Scripts/NoiseDotNet/NoiseSettings.cs
@@ -0,0 +1,52 @@
+namespace NoiseDotNet 
+{
+    /// <summary>
+    /// Settings for noise function calls. 
+    /// </summary>
+    public struct NoiseSettings
+    {
+        /// <summary>
+        /// x-coordinates are multiplied by this number before being passed to the noise function.
+        /// </summary>
+        public float XFrequency;
+
+        /// <summary>
+        /// y-coordinates are multiplied by this number before being passed to the noise function.
+        /// </summary>
+        public float YFrequency;
+
+        /// <summary>
+        /// z-coordinates are multiplied by this number before being passed to the noise function.
+        /// Unused by 2D noise functions.
+        /// </summary>
+        public float ZFrequency;
+
+        /// <summary>
+        /// The output of the noise function is multiplied by this number before being written into the output buffer.
+        /// For cellular noise functions, this applies to the cell center distance output.
+        /// </summary>
+        public float Amplitude;
+
+        /// <summary>
+        /// The output of the noise function is multiplied by this number before being written into the output buffer.
+        /// Not used by gradient noise functions.
+        /// For cellular noise functions, this applied to the cell edge distance output.
+        /// </summary>
+        public float Amplitude2;
+
+        /// <summary>
+        /// The seed for the noise function.
+        /// </summary>
+        public int Seed;
+
+        public void Deconstruct(out float xFrequency, out float yFrequency, out float zFrequency, out float amplitude, out float amplitude2, out int seed)
+        {
+            xFrequency = XFrequency;
+            yFrequency = YFrequency;
+            zFrequency = ZFrequency;
+            amplitude = Amplitude;
+            amplitude2 = Amplitude2;
+            seed = Seed;
+        }
+    }
+}
--- UnityProfiling/Assets/Scripts/Tester.cs
+++ UnityProfiling/Assets/Scripts/Tester.cs
@@ -7,3 +7,4 @@ public class Tester : MonoBehaviour
         
     }
 }
+    
