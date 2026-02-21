85a016e31605858dd95c79863eac218f2cf79a89
Miles <krubbles@gmail.com>
2026-02-21 12:10:38 -0800

    Synced unity version with main version

--- UnityProfiling/Assets/Scripts/NoiseDotNet/NoiseSettings.cs
+++ UnityProfiling/Assets/Scripts/NoiseDotNet/NoiseSettings.cs
@@ -39,11 +39,27 @@ namespace NoiseDotNet
         /// </summary>
         public int Seed;
 
-        public void Deconstruct(out float xFrequency, out float yFrequency, out float zFrequency, out float amplitude, out float amplitude2, out int seed)
+        public NoiseSettings(float xFreq, float yFreq, float zFreq, float amplitude, float amplitude2, int seed)
         {
-            xFrequency = XFrequency;
-            yFrequency = YFrequency;
-            zFrequency = ZFrequency;
+            XFrequency = xFreq;
+            YFrequency = yFreq;
+            ZFrequency = zFreq;
+            Amplitude = amplitude;
+            Amplitude2 = amplitude2;
+            Seed = seed;
+        }
+
+        public NoiseSettings(float xFreq, float yFreq, float amplitude, float amplitude2, int seed) : this(xFreq, yFreq, 0f, amplitude, amplitude2, seed) { }
+
+        public NoiseSettings(float xFreq, float yFreq, int seed) : this(xFreq, yFreq, 0f, 1f, 1f, seed) { }
+
+        public NoiseSettings(float xFreq, float yFreq, float zFreq, int seed) : this(xFreq, yFreq, zFreq, 1f, 1f, seed)  { }
+
+        public void Deconstruct(out float xFreq, out float yFreq, out float zFreq, out float amplitude, out float amplitude2, out int seed)
+        {
+            xFreq = XFrequency;
+            yFreq = YFrequency;
+            zFreq = ZFrequency;
             amplitude = Amplitude;
             amplitude2 = Amplitude2;
             seed = Seed;
