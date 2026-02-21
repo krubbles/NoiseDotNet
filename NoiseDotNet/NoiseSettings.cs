66b31cf61271dded52b31b9e8eccb4bb5dff86ee
Miles <krubbles@gmail.com>
2026-02-21 12:03:37 -0800

    Renamed frequency to freq so argument labling takes less space

--- NoiseDotNet/NoiseSettings.cs
+++ NoiseDotNet/NoiseSettings.cs
@@ -39,27 +39,27 @@ namespace NoiseDotNet
         /// </summary>
         public int Seed;
 
-        public NoiseSettings(float xFrequency, float yFrequency, float zFrequency, float amplitude, float amplitude2, int seed)
+        public NoiseSettings(float xFreq, float yFreq, float zFreq, float amplitude, float amplitude2, int seed)
         {
-            XFrequency = xFrequency;
-            YFrequency = yFrequency;
-            ZFrequency = zFrequency;
+            XFrequency = xFreq;
+            YFrequency = yFreq;
+            ZFrequency = zFreq;
             Amplitude = amplitude;
             Amplitude2 = amplitude2;
             Seed = seed;
         }
 
-        public NoiseSettings(float xFrequency, float yFrequency, float amplitude, float amplitude2, int seed) : this(xFrequency, yFrequency, 0f, amplitude, amplitude2, seed) { }
+        public NoiseSettings(float xFreq, float yFreq, float amplitude, float amplitude2, int seed) : this(xFreq, yFreq, 0f, amplitude, amplitude2, seed) { }
 
-        public NoiseSettings(float xFrequency, float yFrequency, int seed) : this(xFrequency, yFrequency, 0f, 1f, 1f, seed) { }
+        public NoiseSettings(float xFreq, float yFreq, int seed) : this(xFreq, yFreq, 0f, 1f, 1f, seed) { }
 
-        public NoiseSettings(float xFrequency, float yFrequency, float zFrequency, int seed) : this(xFrequency, yFrequency, zFrequency, 1f, 1f, seed)  { }
+        public NoiseSettings(float xFreq, float yFreq, float zFreq, int seed) : this(xFreq, yFreq, zFreq, 1f, 1f, seed)  { }
 
-        public void Deconstruct(out float xFrequency, out float yFrequency, out float zFrequency, out float amplitude, out float amplitude2, out int seed)
+        public void Deconstruct(out float xFreq, out float yFreq, out float zFreq, out float amplitude, out float amplitude2, out int seed)
         {
-            xFrequency = XFrequency;
-            yFrequency = YFrequency;
-            zFrequency = ZFrequency;
+            xFreq = XFrequency;
+            yFreq = YFrequency;
+            zFreq = ZFrequency;
             amplitude = Amplitude;
             amplitude2 = Amplitude2;
             seed = Seed;
